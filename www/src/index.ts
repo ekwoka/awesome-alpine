f909d2f8eaa980e211cf2f918647389409ae4a3e
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2024-01-13 23:53:32 +0400

    :construction: Adds Layout and Types (#1)
    * :construction: Adds header
    
    * :see_no_evil: Ignores APFS files
    
    * Delete .DS_Store
    
    * :lipstick: Gets Basic Layout
    
    * :coffin: Removes old editor sample
    
    * :label: Adds AlpineJS Types to editor

--- www/src/index.ts
+++ www/src/index.ts
@@ -1,7 +1,11 @@
 import { makeEditor, prettify, transpile } from './lib';
+import persist from '@alpinejs/persist';
 import Alpine from 'alpinejs';
+import { editor } from 'monaco-editor';
 
+Alpine.plugin(persist);
 Alpine.data('editor', () => ({
+  panel: Alpine.$persist('html'),
   value: {
     html: `<div x-data=example x-text=text class="text-xl uppercase text-blue-300 flex justify-center items-center">This is the Editor</div>`,
     typescript:
@@ -10,11 +14,11 @@ Alpine.data('editor', () => ({
       "Alpine.data('example', () => ({ text: 'I am the text now!' }))",
   },
   editor: {
-    html: null,
-    typescript: null,
+    html: null as editor.IStandaloneCodeEditor,
+    typescript: null as editor.IStandaloneCodeEditor,
   },
-  get asDocument() {
-    return `<script type="module">import Alpine from '/playSandbox.ts';${this.value.javascript};Alpine.start()</script><link rel="stylesheet" href="/styles.css" />${this.value.html}`;
+  get asDocument(): string {
+    return `<script type="module">import Alpine from '/playSandbox.ts';${this.value.javascript};Alpine.start()</script><link rel="stylesheet" href="/styles.css" /><style>body { background-color: black }</style>${this.value.html}`;
   },
   registerEditor(el: HTMLElement, type: 'html' | 'typescript') {
     this.editor[type] = makeEditor(el, this.value[type], type);
@@ -33,12 +37,17 @@ Alpine.data('editor', () => ({
   },
   async prettify() {
     for (const type in this.value) {
-      this.value[type] = await prettify(this.value[type], 'html');
+      this.value[type] = await prettify(
+        this.value[type],
+        type as unknown as 'html',
+      );
       Alpine.raw(this.editor[type])?.setValue(this.value[type]);
     }
   },
   async update(type: 'html' | 'typescript') {
-    const content = Alpine.raw(this.editor[type])?.getValue();
+    const content = Alpine.raw<(typeof this.editor)[typeof type]>(
+      this.editor[type],
+    )?.getValue();
     this.value[type] = await prettify(content, 'html');
   },
 }));
--- www/src/lib/prettier.ts
+++ www/src/lib/prettier.ts
@@ -4,10 +4,29 @@ const estree = () => import('prettier/plugins/estree.mjs');
 const html = () => import('prettier/plugins/html.mjs');
 const typescript = () => import('prettier/plugins/typescript.mjs');
 
-const getPlugins = (type: 'html' | 'typescript') => {
+export enum Language {
+  HTML = 'html',
+  TYPESCRIPT = 'typescript',
+  JAVASCRIPT = 'javascript',
+}
+
+export const coerceLanguage = (language: Language): Language => {
+  switch (language) {
+    case Language.HTML:
+      return Language.HTML;
+    case Language.TYPESCRIPT:
+    case Language.JAVASCRIPT:
+      return Language.TYPESCRIPT;
+    default:
+      return Language.HTML;
+  }
+};
+
+const getPlugins = (type: Language) => {
   switch (type) {
     case 'html':
       return [html()];
+    case 'javascript':
     case 'typescript':
       return [estree(), typescript()];
     default:
@@ -15,10 +34,11 @@ const getPlugins = (type: 'html' | 'typescript') => {
   }
 };
 
-export const prettify = async (content: string, type: 'html' | 'typescript') =>
+export const prettify = async (content: string, type: Language) =>
   format(content, {
-    parser: type,
+    parser: coerceLanguage(type),
     plugins: await Promise.all(getPlugins(type)),
     singleQuote: true,
     singleAttributePerLine: true,
+    printWidth: 60,
   });
