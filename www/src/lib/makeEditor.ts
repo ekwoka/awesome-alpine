9f688031c3ec841b9e5d277684bff20736b2a7c6
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2024-01-24 22:15:13 +0400

    :lipstick: Changes Theme, and fixes layout sizing issues

--- www/src/lib/makeEditor.ts
+++ www/src/lib/makeEditor.ts
@@ -5,7 +5,10 @@ import * as monaco from 'monaco-editor';
 import DefaultWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
 import HTMLWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
 import TSWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
+import tomorrow from 'monaco-themes/themes/Tomorrow-Night.json';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+monaco.editor.defineTheme('tomorrow-night', tomorrow as any);
 monaco.languages.typescript.typescriptDefaults.addExtraLib(
   `
   ${dts}
@@ -25,17 +28,18 @@ export const makeEditor = (
   el: HTMLElement,
   initialContent: string,
   type: Language,
-) =>
-  monaco.editor.create(el, {
+) => {
+  const editor = monaco.editor.create(el, {
     value: initialContent,
     language: type,
-    theme: 'vs-dark',
-    automaticLayout: true,
+    theme: 'tomorrow-night',
+    automaticLayout: false,
     minimap: {
       enabled: false,
     },
-    lineHeight: 32,
+    lineHeight: 36,
     letterSpacing: 1,
+    fontFamily: 'ml',
     fontLigatures: true,
     fontSize: 16,
     fontVariations: true,
@@ -43,10 +47,18 @@ export const makeEditor = (
       vertical: 'hidden',
       horizontal: 'hidden',
     },
-    wordWrap: 'bounded',
+    wordWrap: 'on',
     wordWrapColumn: 80,
     wrappingIndent: 'indent',
   });
+  const observer = new ResizeObserver(() => {
+    editor.layout({ height: el.offsetHeight, width: el.offsetWidth });
+  });
+
+  observer.observe(el);
+
+  return editor;
+};
 
 window.MonacoEnvironment = {
   getWorker(_, label) {
