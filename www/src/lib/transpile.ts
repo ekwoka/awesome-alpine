20363be2919b2d39304a5ad362472a7eee3802d5
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2024-02-16 23:19:28 +0400

    :zap: Debounces ESM sh transpilations

--- www/src/lib/transpile.ts
+++ www/src/lib/transpile.ts
@@ -1,41 +1,69 @@
 import { CorePlugins } from './lazyModules/alpinePlugins';
-import { build } from 'https://esm.sh/build';
 
-declare module 'https://esm.sh/build' {
-  export type BuildInput = {
-    source: string;
-    loader?: 'js' | 'jsx' | 'ts' | 'tsx';
-    dependencies?: Record<string, string>;
-    types?: string;
-  };
+const { build } = (await import('https://esm.sh/build')) as {
+  build: (input: string | BuildInput) => Promise<BuildOutput>;
+};
 
-  export type TransformOptions = {
-    target?:
-      | 'deno'
-      | 'denonext'
-      | 'node'
-      | 'esnext'
-      | `es201${5 | 6 | 7 | 8 | 9}`
-      | `es202${0 | 1 | 2}`;
-    imports?: Record<string, string>;
-  };
+let timeout: NodeJS.Timeout = null;
+const debouncedBuild = (code: string, config: EditorConfig) => {
+  clearTimeout(timeout);
+  console.log('debouncedBuild');
+  timeout = setTimeout(async () => {
+    const esm = await build({
+      code,
+      loader: config.settings.typescript ? 'ts' : 'js',
+      dependencies: {
+        alpinejs: config.settings.version,
+        ...config.plugins.reduce(
+          (acc, plugin) => {
+            acc[`@alpinejs/${CorePlugins[plugin].toLowerCase()}`] =
+              config.settings.version;
+            return acc;
+          },
+          {} as Record<string, string>,
+        ),
+      },
+    });
+    currentPromise.resolve(esm);
+    currentPromise.promise = null;
+    currentPromise.resolve = null;
+  }, 1000);
+};
 
-  export type BuildOutput = {
-    id: string;
-    url: string;
-    bundleUrl: string;
-  };
-  export const build: (input: string | BuildInput) => Promise<BuildOutput>;
-}
+type BuildInput = {
+  code: string;
+  loader?: 'js' | 'jsx' | 'ts' | 'tsx';
+  dependencies?: Record<string, string>;
+  types?: string;
+};
 
-export const transpile = async (
-  code: string,
-  config: EditorConfig,
-): Promise<{
+type BuildOutput = {
   id: string;
   url: string;
   bundleUrl: string;
-}> => {
+};
+
+const currentPromise: {
+  promise: Promise<ESModule> | null;
+  resolve: ((value: ESModule) => void) | null;
+} = {
+  promise: null,
+  resolve: null,
+};
+
+type ESModule = {
+  id: string;
+  url: string;
+  bundleUrl: string;
+};
+
+export const transpile = (
+  code: string,
+  config: EditorConfig,
+): Promise<ESModule> => {
+  currentPromise.promise ??= new Promise<ESModule>((res) => {
+    currentPromise.resolve = res;
+  });
   const codeWithPlugins = `
   import Alpine from 'alpinejs';
   ${config.plugins.map((plugin) => `import ${CorePlugins[plugin]} from '@alpinejs/${CorePlugins[plugin].toLowerCase()}';`).join('\n')}
@@ -45,21 +73,8 @@ export const transpile = async (
 
   export default Alpine;
   `;
-  return await build({
-    code: codeWithPlugins,
-    loader: config.settings.typescript ? 'ts' : 'js',
-    dependencies: {
-      alpinejs: config.settings.version,
-      ...config.plugins.reduce(
-        (acc, plugin) => {
-          acc[`@alpinejs/${CorePlugins[plugin].toLowerCase()}`] =
-            config.settings.version;
-          return acc;
-        },
-        {} as Record<string, string>,
-      ),
-    },
-  });
+  debouncedBuild(codeWithPlugins, config, currentPromise.resolve);
+  return currentPromise.promise;
 };
 
 type EditorConfig = {
