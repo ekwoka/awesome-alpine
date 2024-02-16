9fab8990e4e712d24110101560a6759eb20f399a
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2024-02-16 23:30:21 +0400

    :construction: Uses ESM.sh to bundle (#17)
    * :construction: Uses ESM.sh to bundle
    
    * :zap: Debounces ESM sh transpilations

--- www/src/components/editor.ts
+++ www/src/components/editor.ts
@@ -1,6 +1,6 @@
-import { prettify } from '../lib';
 import { CorePlugins } from '../lib/lazyModules/alpinePlugins';
 import { RPCReceiver, RPCSender } from '../lib/postmessageRPC';
+import { prettify } from '../lib/prettier';
 import { Language } from '../lib/prettier';
 import type { sandboxActions } from '../play/playSandbox';
 // @ts-expect-error - this is a raw template inmport
@@ -10,7 +10,7 @@ import starterScript from '../play/starter.js?raw';
 import { bitwiseArray, booleanNumber } from '../plugins/encoding';
 import persist from '@alpinejs/persist';
 import query, { base64URL } from '@ekwoka/alpine-history';
-import Alpine from 'alpinejs';
+import Alpine, { PluginCallback } from 'alpinejs';
 import type { editor } from 'monaco-editor';
 
 const makeEditor = () =>
@@ -18,96 +18,104 @@ const makeEditor = () =>
 
 const transpile = () => import('../lib/transpile.js').then((m) => m.transpile);
 
-Alpine.plugin([persist, query]);
-Alpine.data('editor', () => ({
-  panel: Alpine.query('html'),
-  config: {
-    plugins: Alpine.query<CorePlugins[]>([])
-      .as('coreplugins')
-      .encoding(bitwiseArray),
-    settings: {
-      typescript: Alpine.query(false).as('ts').encoding(booleanNumber),
-      tailwind: Alpine.query(false).as('tw').encoding(booleanNumber),
-      version: Alpine.query<`${number}.${number}.${number}`>('3.13.5').as('v'),
-    },
-  },
-  alpineVersions: [] as string[],
-  value: {
-    html: Alpine.query(starterHTML).encoding(base64URL).as('html'),
-    typescript: Alpine.query(starterScript).encoding(base64URL).as('script'),
-  },
-  editor: {
-    html: null as editor.IStandaloneCodeEditor,
-    typescript: null as editor.IStandaloneCodeEditor,
-  },
-  sandbox: null as RPCSender<sandboxActions> | null,
-  async registerEditor(el: HTMLElement, type: Language) {
-    this.editor[type] = (await makeEditor())(el, this.value[type], type);
-  },
-  init() {
-    console.log('Initializing Alpine');
-    new RPCReceiver({
-      registerSandbox: (event?: MessageEvent<undefined>) => {
-        if (event?.source)
-          this.initializeSandbox(
-            new RPCSender<sandboxActions>(event.source as Window),
-          );
-        return true;
+export const Editor: PluginCallback = (Alpine) => {
+  Alpine.plugin([persist, query]);
+  console.log('setting editor data');
+  Alpine.data('editor', () => {
+    let abortController: AbortController = null;
+    return {
+      panel: Alpine.query('html'),
+      config: {
+        plugins: Alpine.query<CorePlugins[]>([])
+          .as('coreplugins')
+          .encoding(bitwiseArray),
+        settings: {
+          typescript: Alpine.query(false).as('ts').encoding(booleanNumber),
+          tailwind: Alpine.query(false).as('tw').encoding(booleanNumber),
+          version:
+            Alpine.query<`${number}.${number}.${number}`>('3.13.5').as('v'),
+        },
       },
-    });
-  },
-  async initializeSandbox(sandbox: RPCSender<sandboxActions>) {
-    this.sandbox = sandbox;
-    console.log('initializing sandbox');
-    this.sandbox.call.log('Hello from Alpine!');
-    await effectPromise(async () => {
-      console.log('loading Alpine version', this.config.settings.version);
-      await this.sandbox.call.loadAlpine(this.config.settings.version);
-    });
-    await Promise.all([
-      effectPromise(async () => {
-        console.log('Loading Tailwind');
-        if (this.config.settings.tailwind)
-          await this.sandbox.call.loadTailwind();
-      }),
-      effectPromise(async () => {
-        console.log('Adding Plugins');
-        await this.sandbox.call.loadPlugins(Alpine.raw(this.config.plugins));
-      }),
-      effectPromise(async () => {
-        await this.sandbox.call.evaluateScript(
-          this.config.settings.typescript && this.value.typescript
-            ? await (
-                await transpile()
-              )(this.value.typescript)
-            : this.value.typescript,
-        );
-      }),
-      effectPromise(async () => {
-        await this.sandbox.call.replaceMarkup(this.value.html);
-      }),
-    ]);
-    this.sandbox.call.start();
-    const alpineRegistry = await import(
-      'registry.npmjs.com/alpinejs?dlx&json'
-    ).then((mod) => mod.default);
-    console.log(alpineRegistry);
-    this.alpineVersions = Object.keys(alpineRegistry.versions).reverse();
-  },
-  async prettify() {
-    for (const type of [Language.HTML, Language.TYPESCRIPT]) {
-      this.value[type] = await prettify(this.value[type], type as Language);
-      Alpine.raw(await this.editor[type])?.setValue(this.value[type]);
-    }
-  },
-  async update(type: Language.HTML | Language.TYPESCRIPT) {
-    const content = Alpine.raw<(typeof this.editor)[typeof type]>(
-      this.editor[type],
-    )?.getValue();
-    this.value[type] = await prettify(content, type);
-  },
-  CorePlugins: Object.entries(CorePlugins).filter(([key]) => isNaN(+key)),
-}));
+      alpineVersions: [] as string[],
+      value: {
+        html: Alpine.query(starterHTML).encoding(base64URL).as('html'),
+        typescript: Alpine.query(starterScript)
+          .encoding(base64URL)
+          .as('script'),
+      },
+      editor: {
+        html: null as editor.IStandaloneCodeEditor,
+        typescript: null as editor.IStandaloneCodeEditor,
+      },
+      sandbox: null as RPCSender<sandboxActions> | null,
+      async registerEditor(el: HTMLElement, type: Language) {
+        this.editor[type] = (await makeEditor())(el, this.value[type], type);
+      },
+      init() {
+        console.log('Initializing Alpine');
+        new RPCReceiver({
+          registerSandbox: (event?: MessageEvent<undefined>) => {
+            if (event?.source)
+              this.initializeSandbox(
+                new RPCSender<sandboxActions>(event.source as Window),
+              );
+            return true;
+          },
+        });
+      },
+
+      async initializeSandbox(sandbox: RPCSender<sandboxActions>) {
+        this.sandbox = sandbox;
+        console.log('initializing sandbox');
+        this.sandbox.call.log('Hello from Alpine!');
+        await Promise.all([
+          effectPromise(async () => {
+            console.log('Loading Tailwind');
+            if (this.config.settings.tailwind)
+              await this.sandbox.call.loadTailwind();
+          }),
+          effectPromise(async () => {
+            if (abortController) abortController.abort();
+            abortController = null;
+            console.log('effect running');
+            if (!(JSON.stringify(this.config) && this.value.typescript)) return;
+            console.log('transpiling');
+            abortController = new AbortController();
+            const signal = abortController.signal;
+            const esm = await (
+              await transpile()
+            )(this.value.typescript, this.config);
+            if (signal.aborted) return;
+            abortController = null;
+            await this.sandbox.call.loadScript(esm);
+          }),
+        ]);
+        await effectPromise(async () => {
+          await this.sandbox.call.replaceMarkup(this.value.html);
+        }),
+          this.sandbox.call.start();
+        const alpineRegistry = await import(
+          'registry.npmjs.com/alpinejs?dlx&json'
+        ).then((mod) => mod.default);
+        console.log(alpineRegistry);
+        this.alpineVersions = Object.keys(alpineRegistry.versions).reverse();
+      },
+      async prettify() {
+        for (const type of [Language.HTML, Language.TYPESCRIPT]) {
+          this.value[type] = await prettify(this.value[type], type as Language);
+          Alpine.raw(await this.editor[type])?.setValue(this.value[type]);
+        }
+      },
+      async update(type: Language.HTML | Language.TYPESCRIPT) {
+        const content = Alpine.raw<(typeof this.editor)[typeof type]>(
+          this.editor[type],
+        )?.getValue();
+        this.value[type] = await prettify(content, type);
+      },
+      CorePlugins: Object.entries(CorePlugins).filter(([key]) => isNaN(+key)),
+    };
+  });
+};
 
 declare global {
   interface Window {
--- www/src/lib/transpile.ts
+++ www/src/lib/transpile.ts
@@ -1,15 +1,87 @@
-import * as esbuild from 'esbuild-wasm';
-import esbuildWASM from 'esbuild-wasm/esbuild.wasm?url';
+import { CorePlugins } from './lazyModules/alpinePlugins';
 
-let esbuildWorker = null;
+const { build } = (await import('https://esm.sh/build')) as {
+  build: (input: string | BuildInput) => Promise<BuildOutput>;
+};
 
-const createWorker = () =>
-  esbuild.initialize({
-    wasmURL: esbuildWASM,
+let timeout: NodeJS.Timeout = null;
+const debouncedBuild = (code: string, config: EditorConfig) => {
+  clearTimeout(timeout);
+  console.log('debouncedBuild');
+  timeout = setTimeout(async () => {
+    const esm = await build({
+      code,
+      loader: config.settings.typescript ? 'ts' : 'js',
+      dependencies: {
+        alpinejs: config.settings.version,
+        ...config.plugins.reduce(
+          (acc, plugin) => {
+            acc[`@alpinejs/${CorePlugins[plugin].toLowerCase()}`] =
+              config.settings.version;
+            return acc;
+          },
+          {} as Record<string, string>,
+        ),
+      },
+    });
+    currentPromise.resolve(esm);
+    currentPromise.promise = null;
+    currentPromise.resolve = null;
+  }, 1000);
+};
+
+type BuildInput = {
+  code: string;
+  loader?: 'js' | 'jsx' | 'ts' | 'tsx';
+  dependencies?: Record<string, string>;
+  types?: string;
+};
+
+type BuildOutput = {
+  id: string;
+  url: string;
+  bundleUrl: string;
+};
+
+const currentPromise: {
+  promise: Promise<ESModule> | null;
+  resolve: ((value: ESModule) => void) | null;
+} = {
+  promise: null,
+  resolve: null,
+};
+
+type ESModule = {
+  id: string;
+  url: string;
+  bundleUrl: string;
+};
+
+export const transpile = (
+  code: string,
+  config: EditorConfig,
+): Promise<ESModule> => {
+  currentPromise.promise ??= new Promise<ESModule>((res) => {
+    currentPromise.resolve = res;
   });
+  const codeWithPlugins = `
+  import Alpine from 'alpinejs';
+  ${config.plugins.map((plugin) => `import ${CorePlugins[plugin]} from '@alpinejs/${CorePlugins[plugin].toLowerCase()}';`).join('\n')}
+  ${config.plugins.map((plugin) => `Alpine.plugin(${CorePlugins[plugin]});`).join('\n')}
 
-export const transpile = async (content: string) => {
-  await (esbuildWorker ??= createWorker());
-  const result = await esbuild.transform(content, { loader: 'ts' });
-  return result.code;
+  ${code}
+
+  export default Alpine;
+  `;
+  debouncedBuild(codeWithPlugins, config, currentPromise.resolve);
+  return currentPromise.promise;
+};
+
+type EditorConfig = {
+  plugins: CorePlugins[];
+  settings: {
+    typescript: boolean;
+    tailwind: boolean;
+    version: `${number}.${number}.${number}`;
+  };
 };
