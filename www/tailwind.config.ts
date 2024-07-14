eb12619d816051bcc18b12f3ed40a3c4479ccc1a
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2024-07-14 18:25:50 +0400

    :memo: Writes up post of the memory leak

--- www/tailwind.config.ts
+++ www/tailwind.config.ts
@@ -2,7 +2,7 @@ import Typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
 
 export default {
-  content: ['./src/**/*.{html,ts,tsx,svg,astro,mdx}'],
+  content: ['./src/**/*.{html,ts,tsx,svg,astro,md,mdx}'],
   theme: {
     extend: {},
   },
