c3378310b5bc2ef7d597faa57d6b5f1f820722af
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2024-07-18 21:39:57 +0400

    :construction: Adds Cloudflare adaptor

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -2,43 +2,42 @@ import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 import viteConfig from './vite.config.ts';
-import {visit} from 'unist-util-visit'
+import { visit } from 'unist-util-visit';
 
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   vite: viteConfig,
   image: {
-    domains: ['loremflickr.com'],
+    domains: ['loremflickr.com']
   },
-  integrations: [
-    tailwind({
-      configFile: './tailwind.config.ts',
-      nesting: true,
-      applyBaseStyles: false,
-    }),
-    mdx({ optimize: true,
-     }),
-  ],
+  integrations: [tailwind({
+    configFile: './tailwind.config.ts',
+    nesting: true,
+    applyBaseStyles: false
+  }), mdx({
+    optimize: true
+  })],
   markdown: {
     shikiConfig: {
       wrap: true,
-      theme: 'one-dark-pro',
+      theme: 'one-dark-pro'
     },
-    rehypePlugins: [addSpacesToCode],
-
+    rehypePlugins: [addSpacesToCode]
   },
   server: {
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin',
       'Cross-Origin-Embedder-Policy': 'require-corp',
-      'Cross-Origin-Resource-Policy': 'same-site',
-    },
+      'Cross-Origin-Resource-Policy': 'same-site'
+    }
   },
+  output: "server",
+  adapter: cloudflare()
 });
-
 function addSpacesToCode() {
-  return (tree) => visit(tree, 'element', (node, _index, parent) => {
-    if (node.tagName === 'code' && parent?.tagName?.startsWith('h') && node.children[0]?.type === 'text')
-      node.children[0].value = node.children[0].value.replaceAll(/[.})<]/g, (match) => `​${match}`).replaceAll(/[:{(>]/g, (match) => `${match}​`)
-  })}
+  return tree => visit(tree, 'element', (node, _index, parent) => {
+    if (node.tagName === 'code' && parent?.tagName?.startsWith('h') && node.children[0]?.type === 'text') node.children[0].value = node.children[0].value.replaceAll(/[.})<]/g, match => `​${match}`).replaceAll(/[:{(>]/g, match => `${match}​`);
+  });
+}
