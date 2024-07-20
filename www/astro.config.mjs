043306d21b4d5727d8be2251436df0ad2e7c1b53
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2024-07-20 17:13:19 +0400

    :construction: Uses Cloudflare Workers for editor page

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -34,7 +34,7 @@ export default defineConfig({
     }
   },
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare({ platformProxy: { enabled: true }, imageService: 'compile' })
 });
 function addSpacesToCode() {
   return tree => visit(tree, 'element', (node, _index, parent) => {
